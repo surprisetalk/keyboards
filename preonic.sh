bf656d16050c050f1d9d86a9d6ad5769f0a91089
Taylor Sarrafian <surprisetalk@gmail.com>
2018-03-27 21:00:26 -0700

    script updates

--- preonic.sh
+++ preonic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd qmk_firmware && \
-make planck/rev4:surprisetalk && \
+make preonic/rev2:surprisetalk && \
 echo "Please put your keyboard in RESET mode.\n" && \
 sudo sleep 10 && \
 sudo dfu-programmer atmega32u4 erase && \
-sudo dfu-programmer atmega32u4 flash .build/planck_rev4_surprisetalk.hex && \
+sudo dfu-programmer atmega32u4 flash .build/preonic_rev2_surprisetalk.hex && \
 sudo dfu-programmer atmega32u4 start && \
 cd ..
