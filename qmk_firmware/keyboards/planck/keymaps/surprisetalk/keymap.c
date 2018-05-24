4e8c43eb2653d42298027ff739d946a86866a0a5
Taylor Sarrafian <surprisetalk@gmail.com>
2018-05-24 07:57:36 -0700

    qwertying

--- qmk_firmware/keyboards/planck/keymaps/surprisetalk/keymap.c
+++ qmk_firmware/keyboards/planck/keymaps/surprisetalk/keymap.c
@@ -68,15 +68,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Bksp |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  ]   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   {  |   [  |   -  |  Esc | Bksp |Space |Enter |   )  |   =  |   }  |  \   |
+ * | Tab  |   {  |   -  |   (  |  Esc | Bksp |Space |Enter |   )  |   =  |   }  |  \   |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   // BUG: a lot of these are wrong when translating to dvorak...
-  {ALT_T(KC_GRV ), KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , ALT_T(KC_SLSH)},
+  {ALT_T(KC_GRV ), KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , ALT_T(KC_LBRC)},
   {CTL_T(KC_ESC ), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, CTL_T(KC_MINS)},
-  {SFT_T(KC_BSPC), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SFT_T(KC_EQL )},
-  {GUI_T(KC_TAB ), KC_LCBR, KC_LBRC, KC_LPRN, KC_LGUI, LOWER  , KC_SPC , TH_ENT , KC_RPRN, KC_RBRC, KC_RCBR, GUI_T(KC_BSLS)}
+  {SFT_T(KC_BSPC), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SFT_T(KC_RBRC)},
+  {GUI_T(KC_TAB ), KC_LCBR, KC_MINS, KC_LPRN, KC_LGUI, LOWER  , KC_SPC , TH_ENT , KC_RPRN, KC_EQL , KC_RCBR, GUI_T(KC_BSLS)}
 },
 
 /* Lower
