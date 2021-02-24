7be3dc184c7a6f32a7def94ba20d71c5c618753a
Taylor Sarrafian <surpristealk@gmail.com>
2021-02-23 22:07:10 -0800

    👑

--- planck.sh
+++ planck.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# rev4 -> rev6
+# planck/rev4:surprisetalk <-> planck/rev6:surprisetalk:dfu-util 
+#   note the :dfu-util at the end
 
 cd qmk_firmware && \
-make planck/rev6:surprisetalk:dfu-util && \
+make planck/rev4:surprisetalk && \
 echo "Please put your keyboard in RESET mode.\n" && \
 sudo sleep 10 && \
 sudo dfu-programmer atmega32u4 erase && \
