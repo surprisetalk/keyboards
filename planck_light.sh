79c84a3017f12bce0f615a111d2203237ac4a798
Taylor Sarrafian <surpristealk@gmail.com>
2021-06-09 18:34:24 -0700

    🎃

--- planck_light.sh
+++ planck_light.sh
@@ -4,7 +4,7 @@ cd qmk_firmware && \
 make planck/light:surprisetalk && \
 echo "Please put your keyboard in RESET mode.\n" && \
 sudo sleep 10 && \
-sudo dfu-programmer at90usb1286 erase && \
+sudo dfu-programmer at90usb1286 erase --force && \
 sudo dfu-programmer at90usb1286 flash .build/planck_light_surprisetalk.hex && \
 sudo dfu-programmer at90usb1286 start && \
 cd ..
